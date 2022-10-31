b485454b2fe325c2f8182641b04f6a8525d518dd
Floris ten Hove <f.tenhove@trengo.com>
2022-10-31 14:44:21 +0100

    Add docblock type annotations to make static analysis tools understand magic methods in Facade

--- src/Facades/Unleash.php
+++ src/Facades/Unleash.php
@@ -4,6 +4,14 @@ namespace JWebb\Unleash\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static array setClient(Unleash\Client\Unleash $client)
+ * @method static bool isEnabled(string $featureName, ?\Unleash\Client\Configuration\Context $context = null, bool $default = false)
+ * @method static array getFeatures(bool $onlyEnabled = false, ?\Unleash\Client\Configuration\Context $context = null)
+ * @method static \Unleash\Client\DTO\Variant getVariant(string $featureName, ?\Unleash\Client\Configuration\Context $context = null, ?\Unleash\Client\DTO\Variant $fallbackVariant = null)
+ * @method static void register()
+ * @method static void getRepository()
+ */
 class Unleash extends Facade
 {
     /**
